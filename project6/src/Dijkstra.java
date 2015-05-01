b8d59dd1a01f736c76d915aa625af67111b4e91a
Prashant Gupta <prashant11gupta@gmail.com>
2015-05-01 13:31:01 -0500

    Update Dijkstra.java

--- project6/src/Dijkstra.java
+++ project6/src/Dijkstra.java
@@ -2,31 +2,74 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
-class Vertex implements Comparable<Vertex> {
-	public static Vertex[] split1;
-	public final String name;
-	public Edge[] adjancies;
-	public double minDistance = Double.POSITIVE_INFINITY;
-	public Vertex previous;
+class Storage {
+	private Map<String, Node> map;
 
-	public Vertex(String argsName) {
-		name = argsName;
+	public Storage() {
+		map = new HashMap<String, Node>();
 	}
 
-	@Override
-	public int compareTo(Vertex other) {
-		return Double.compare(minDistance, other.minDistance);
+	public Node getNode(String name) {
+		if (map.containsKey(name)) {
+			return map.get(name);
+		}
+		return null;
+
+	}
+
+	public Node addNode(String name) {
+		if (map.containsKey(name)) {
+			return map.get(name);
+
+		} else {
+			Node n = new Node(name);
+			map.put(name, n);
+			return n;
+		}
+
+	}
+
+	public Collection<Node> getAllNodes() {
+		return map.values();
 	}
 
 }
 
-class Edge {
-	public String target;
-	public double weight;
+class Node implements Comparable<Node> {
+	public final String name;
+	public List<Edge> adjancies;
+	public Integer minDistance = Integer.MAX_VALUE;
+	public Node previousNode;
+	boolean known;
 
-	public Edge(String destVertex, double weight) {
-		this.target = destVertex;
+	public Node(String argsName) {
+		name = argsName;
+		this.adjancies = new LinkedList<Edge>();
+	}
+
+	@Override
+	public int compareTo(Node other) {
+		return Double.compare(this.minDistance, other.minDistance);
+
+	}
+}
+
+class Edge {
+	public Node destNode;
+	public Integer weight;
+
+	public Edge(Node destNode, Integer weight) {
+		this.destNode = destNode;
 		this.weight = weight;
 
 	}
@@ -34,61 +77,133 @@ class Edge {
 
 public class Dijkstra {
 
-	public void readInputFile(String file_name) {
-		try {
-			FileReader file = new FileReader(file_name);
-			BufferedReader fr = new BufferedReader(file);
+	public static void findPaths(Node source, Collection<Node> allNodes) {
+		for (Node n : allNodes) {
+			n.minDistance = Integer.MAX_VALUE;
+			n.previousNode = null;
+		}
 
-			try {
-				String text = null;
-				String srcVertex = null;
-				String destVertex = null;
-				double weight = 0;
-				while ((text = fr.readLine()) != null) {
-					String[] split = text.split("  ");
-					srcVertex = split[0];
-					Vertex v = new Vertex(srcVertex);
-					System.out.print("Source: " + srcVertex);
-					for (int i = 1; i < split.length; i++) {
-						String[] split1 = split[i].split(" ");
-						destVertex = split1[0];
-						weight = Double.parseDouble(split1[1]);
+		source.minDistance = 0;
+		PriorityQueue<Node> NodeQueue = new PriorityQueue<Node>();
+		NodeQueue.add(source);
 
-						System.out.print(" Destination: " + destVertex);
-						System.out.print(" Weight: " + weight);
-					}
-					v.adjancies = new Edge[] { new Edge(destVertex, weight) };
-					for (Edge e : v.adjancies) {
-						System.out.println();
-						System.out.println(e.target);
-						System.out.println(e.weight);
-					}
+		while (!NodeQueue.isEmpty()) {
+			Node u = NodeQueue.poll();
+
+			// Visit each edge exiting u
+			for (Edge e : u.adjancies) {
+				Node v = e.destNode;
+				Integer weight = e.weight;
+				Integer distanceThroughU = u.minDistance + weight;
+				if (distanceThroughU < v.minDistance) {
+					NodeQueue.remove(v);
+					v.minDistance = distanceThroughU;
+					v.previousNode = u;
+					NodeQueue.add(v);
 				}
-
-			} finally {
-				fr.close();
 			}
 		}
+	}
 
-		catch (FileNotFoundException e) {
+	public static List<String> getShortestPath(Node destNode) {
+		List<String> path = null;
+		path = new ArrayList<String>();
+		for (Node node = destNode; node != null; node = node.previousNode)
+			path.add(node.name);
+		Collections.reverse(path);
+		return path;
+	}
+
+	public static Storage readInputFile(String file_name) throws IOException {
+		Storage storage = new Storage();
+		BufferedReader bufferedReader = null;
+		try {
+			FileReader file = new FileReader(file_name);
+			bufferedReader = new BufferedReader(file);
+
+			String text = null;
+			String srcVertex = null;
+			String destVertex = null;
+			Integer weight = null;
+			Node sourceNode = null;
+			Node destNode = null;
+
+			while ((text = bufferedReader.readLine()) != null) {
+				String[] split = text.split("  ");
+				srcVertex = split[0];
+				sourceNode = storage.addNode(srcVertex);
+
+				for (int i = 1; i < split.length; i++) {
+
+					String[] split1 = split[i].split(" ");
+					destVertex = split1[0];
+					weight = Integer.parseInt(split1[1]);
+					destNode = storage.addNode(destVertex);
+					Edge e = new Edge(destNode, weight);
+					sourceNode.adjancies.add(e);
+				}
+
+			}
+
+			return storage;
+		} catch (FileNotFoundException e) {
 			System.out.println("File Not Found " + file_name);
 		} catch (IOException e) {
-			System.out.println("Unable to read file " + file_name);
+			e.printStackTrace();
+		} finally {
+			bufferedReader.close();
 		}
+		return null;
 	}
 
 	public static void main(String[] args) {
 
+		String yesOrNo = "Y";
 		String file_name = "airports.txt";
+		try {
+			Storage storage = readInputFile(file_name);
+			try (Scanner sc = new Scanner(System.in)) {
+				do {
 
-		Dijkstra dj = new Dijkstra();
-		/*
-		 * Scanner sc = new Scanner(System.in);
-		 * System.out.print("Enter departure airport: "); String dept =
-		 * sc.next(); System.out.print("Enter arrival airport: "); String arvl =
-		 * sc.next(); sc.close();
-		 */
-		dj.readInputFile(file_name);
+					System.out.println("---------------------");
+					System.out.print("Enter departure airport: ");
+					String dept = sc.next();
+					System.out.print("Enter arrival airport: ");
+					String arvl = sc.next();
+					System.out.println("---------------------");
+					Node src = storage.getNode(dept);
+					Node dest = storage.getNode(arvl);
+					if (src == null | dest == null) {
+						System.out
+								.println("Please enter correct name, Airport entered does not exist");
+						continue;
+					}
+					findPaths(src, storage.getAllNodes());
+					System.out.println();
+					System.out.println("By Price:");
+					System.out.println("  Price:       " + dest.minDistance);
 
+					List<String> path = getShortestPath(dest);
+					System.out.println("  Connections: " + (path.size() - 2));
+					System.out.print("  Route:       ");
+					int i = 1;
+					for (String connections : path) {
+						System.out.print(connections);
+						if (i < path.size()) {
+							System.out.print("->");
+							i++;
+						}
+					}
+					System.out.println(" Check another route (Y/N)?");
+					yesOrNo = sc.next("\\w{1}");
+
+				} while ("Y".equalsIgnoreCase(yesOrNo));
+			}
+			System.out.println("Thank you very much for using Application!!!");
+		} catch (NullPointerException e) {
+			System.out.println();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
